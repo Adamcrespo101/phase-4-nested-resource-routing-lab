fb8f56840be07e98f13fb864e27761a367e5e061
Adam Crespo <alc49@LAPTOP-7KRCGAGF.localdomain>
2022-04-27 18:43:48 -0400

    commit tree 918b71ab44f06953a409eea017439bef8928cd12

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,41 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :err_response
+before_action :find_user
+
 
   def index
-    items = Item.all
+    if @user
+    items = @user.items
+    else 
+      items = Item.all
+    end
     render json: items, include: :user
-  end
+end
+
+def show 
+item = Item.find(params[:id])
+render json: item, status: 200
+end
+
+def create 
+  user = @user
+item = user.items.create(item_params)
+render json: item, status: :created
+end
+
+private
+
+def find_user 
+  @user = User.find(params[:user_id])
+end
+
+def err_response
+  render json: {message: "user not found"}, status: :not_found
+end
+
+def item_params
+  params.permit(:name, :description, :price, :user_id)
+end
+
 
 end
